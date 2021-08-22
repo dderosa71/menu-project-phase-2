6e625100210d8265692b7c1e55e39ac60113e820
Dominic DeRosa <dderosa71@gmail.com>
2021-08-22 15:44:13 +0200

    keys set on menu items

--- your-app-name/src/Menu.js
+++ your-app-name/src/Menu.js
@@ -6,6 +6,7 @@ function Menu({menuItems}){
 
     const menuArray = menuItems.map(item=>(
         <MenuItem 
+        key={item.id}
         name={item.name} 
         description={item.description}
         price={item.price}
