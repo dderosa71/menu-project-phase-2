99a1bb3122faf1fb33ba55154fc52e0180463244
Dominic DeRosa <dderosa71@gmail.com>
2021-08-23 07:17:00 -0500

    added browser router

--- your-app-name/src/App.js
+++ your-app-name/src/App.js
@@ -5,10 +5,7 @@ import React, { useState, useEffect } from 'react';
 import Header from "./Headers.js"
 import AddNewItem from './AddNewItem';
 import CustomerPage from "./CustomerPage.js"
-// import { BrowserRouter, Route } from "react-router-dom";
-
-
-
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
 function App() {
 
@@ -23,39 +20,23 @@ function App() {
   const [menuItems, setMenuItems] = useState([])
   const [newItemScreen, setNewItemScreen] = useState(false)
   const displayMe = newItemScreen ? "hidden" : "shown"
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <>
-
-    {/* <BrowserRouter>
-    <Route path="/"> */}
-     <div className={displayMe}>
-        <Header  setNewItemScreen={setNewItemScreen}  />
-        <Menu menuItems={menuItems} jsonURL={jsonURL} setMenuItems={setMenuItems} />
-      </div>
-    {/* </Route>
-  </BrowserRouter>, */}
-     
-      <div className={displayMe} >
-        <AddNewItem setMenuItems={setMenuItems} jsonURL={jsonURL}/>
-        <CustomerPage menuItems={menuItems}/>
-      </div>
-
+      <BrowserRouter>
+        <Route path="/">
+          <Header setNewItemScreen={setNewItemScreen} />
+        </Route>
+        <Route path="/menu">
+          <Menu menuItems={menuItems} jsonURL={jsonURL} setMenuItems={setMenuItems} />
+        </Route>
+        <Route path="/AddNewItem">
+          <AddNewItem setMenuItems={setMenuItems} jsonURL={jsonURL} />
+        </Route>
+        <Route path="/CustomerMenu">
+          <CustomerPage menuItems={menuItems} />
+        </Route>
+      </BrowserRouter>,
     </>
   );
 }
